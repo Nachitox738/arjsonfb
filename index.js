dadd7a3e6f02260ae345e430d2d6994d17a520df
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-18 01:08:34 -0300

    adsggsda

--- index.js
+++ index.js
@@ -1,75 +1,97 @@
 //modulos
 const express = require('express');
-const fs = require('fs'); //Permite trabajar con archivos (file system) incluida con node, no se instala
-const cors = require('cors');
-require('/config');
+const fs = require('fs') //Permite trabajar con archivos (file system) incluida con node, no se instala
+const cors = require('cors')
 const app = express();
-const port = process.env.MYSQL_ADDON_PORT || 3000;
+const port = 3000;
+
 
 //Middleware
 app.use(express.json())
 app.use(express.static('./public')) //Ejecuta directamente el front al correr el servidor
 app.use(cors())
 
-app.get('/productos', (req, res) => {
-   const sql = "SELECT * FROM articulos";
-   db.query(sql,(err,result)=>{
-    if(err){
-        console.error("Error de lectura");
-        return;
+
+//Función para leer los datos del archivo .json
+
+const leerDatos = () => {
+    try {   //intenta convertir cadena, si no funciona nos muestra por consol el error (catch)
+        const datos = fs.readFileSync('public/data/datos.json')
+
+        return JSON.parse(datos); // Convierte una cadena JSON en un objeto JavaScript
+    } catch (error) {
+        console.log(error)
     }
-    res.json(result);
-   })
+}
+//leerDatos()
+const escribirDatos = (datos) => {
+    try {
+        fs.writeFileSync('public/data/datos.json', JSON.stringify(datos)) //writeFile permite escribir datos || JSON.stringify convierte un objeto JS en JSON
+
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+
+
+app.get('/productos', (req, res) => {
+   const datos= leerDatos();
+   res.json(datos.productos);
 })
 
 app.get('/productos/:id', (req, res) => {
     const datos = leerDatos();
-    const prodEncontrado = datos.productos.find ((p) => p.id == req.params.id)
+    const prodEncontrado= datos.productos.find ((p) => p.id == req.params.id)
     if (!prodEncontrado) { // ! (no) o diferente
         return res.status(404).json(`No se encuentra el producto`)
     }
     res.json({
-        mensaje: "Producto encontrado",
+        mensaje: "producto encontrado",
         producto: prodEncontrado
     })
 })
 
 app.post('/productos', (req, res) => {
-    const sql = "INSERT INTO articulos (nombre, descripcion, precio, imagen) VALUES (?,?,?,?)";
-    const values = Object.values(req.body);
-    db.query(sql,values,(err,result)=>{
-        if(err){
-            console.error("Error al guardar", err);
-            return;
-        }
-        res.json({mensaje:"Nuevo producto agregado"});
-       })
+    const datos= leerDatos();
+    nuevoProducto = { id: datos.productos.length + 1, ...req.body }     //Genera un ID y agrega una copia de req.body
+    datos.productos.push(nuevoProducto)
+    escribirDatos(datos);
+    res.json({"mensaje":'Nuevo producto agregado',
+        producto: nuevoProducto});
     })
 
 
-app.put('/productos', (req, res) => {
-    const valores = Object.values(req.body);
-    const sql = "UPDATE articulos SET nombre=?, descripcion=?, precio=? WHERE id=?";
-    db.query(sql,valores,(err,result)=>{
-        if(err){
-            console.error("Error al modificar: ",err);
-            return;
-        }
-        res.json({mensaje:'Producto actualizado', data:result});
-    })
+app.put('/productos/:id', (req, res) => {
+    const id= req.params.id;
+    const nuevosDatos= req.body;
+    const datos = leerDatos()
+    const prodEncontrado=datos.productos.find((p)=>p.id ==req.params.id)
+    console.log(prodEncontrado)
+    if (!prodEncontrado) {
+        return res.status(404).json({mensaje:"No se encontró el producto"})
+    }
+    datos.productos = datos.productos.map(p => p.id == req.params.id ? { ...p, ...nuevosDatos } : p)
+    escribirDatos(datos)
+    res.json({mensaje:"Producto Actualizado"})
 })
 
 app.delete('/productos/:id', (req, res) => {
-    const id = req.params.id;
-    sql = "DELETE FROM articulos WHERE id=?";
-    db.query(sql,[id],(err,result)=>{
-        if(err){
-            console.error("Error al borrar", err);
-            return;
-        }
-        res.json({mensaje:"Producto borrado"});
-       })
-
+    const id=req.params.id;
+    const datos= leerDatos()
+    const prodEncontrado = datos.productos.find((p) => p.id == req.params.id)
+    if (!prodEncontrado) {
+        return res.status(404).json(`No se encuentra el producto`)
+    }
+    datos.productos= datos.productos.filter((p)=>p.id != req.params.id)
+    let indice=1
+    datos.productos.map((p)=>{
+        p.id=indice
+        indice++
+    })
+    escribirDatos(datos)
+    res.json({"mensaje":"Producto Eliminado"}) 
 })
 
 app.listen(port, () => {
