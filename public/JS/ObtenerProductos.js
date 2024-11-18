651e6f785e46f3bd11c9eb4deb2a7cd38aceb5af
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-18 01:47:47 -0300

    astslost

--- public/JS/ObtenerProductos.js
+++ public/JS/ObtenerProductos.js
@@ -13,8 +13,8 @@ const mostrarProductos = (datos) => {
                 <h5 class="card-title">${dato.nombre}</h5>
                 <p class="card-text">${dato.descripcion}</p>
                 <p class="card-text">$${dato.precio}</p>   
-                 <div style="margin-left:20px;">
-                <a href="#" class="btn btn-primary">Aprende Mas</a>
+                 <div style="margin-left:20px; justify-content:center;">
+                <a href="#" class="btn btn-primary" style:"width: 150px;">Comprar</a>
               </div>
               </div>
         </div>
